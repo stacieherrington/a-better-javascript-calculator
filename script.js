ec4e53de4a3903b0c1fb857de3f42cffb564f738
[Stacie Herrington] <stacieherrington@gmail.com>
2019-01-24 12:33:45 -0600

    calculator numbers and operators now work. still needs AC and = buttons.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currentTotal = 0
 let digitsString = ""
+let operationType = ""
 
 
 document.getElementById("button7").addEventListener("click", function(){
@@ -58,14 +59,80 @@ document.getElementById("buttonDecimal").addEventListener("click", function(){
     console.log(digitsString);
 });
 
+document.getElementById("buttonAdd").addEventListener("click", function(){
+    if (digitsString !== "") {
+        currentTotal = operatorButtonStuff(operationType, digitsString)
+    }
+    console.log(currentTotal)
+    digitsString = ""
+    operationType = "operationAdd"
+});
 
+document.getElementById("buttonSubtract").addEventListener("click", function(){
+    if (digitsString !== "") {
+        currentTotal = operatorButtonStuff(operationType, digitsString)
+    }
+    console.log(currentTotal)
+    digitsString = ""
+    operationType = "operationSubtract"
+});
+
+document.getElementById("buttonMultiply").addEventListener("click", function(){
+    if (digitsString !== "") {
+        currentTotal = operatorButtonStuff(operationType, digitsString)
+    }
+    console.log(currentTotal)
+    digitsString = ""
+    operationType = "operationMultiply"
+});
+
+document.getElementById("buttonDivide").addEventListener("click", function(){
+    if (digitsString !== "") {
+        currentTotal = operatorButtonStuff(operationType, digitsString)
+    }
+    console.log(currentTotal)
+    digitsString = ""
+    operationType = "operationDivide"
+});
+
+function performOperation(lastResult, newNumberEntered, arithmeticOperation) {
+    switch(arithmeticOperation) {
+        case "operationAdd":
+            return(lastResult + newNumberEntered)
+            break;
+        case "operationSubtract":
+            return(lastResult - newNumberEntered)
+            break;
+        case "operationMultiply":
+            return(lastResult * newNumberEntered)
+            break;
+        case "operationDivide":
+            return(lastResult / newNumberEntered)
+            break;
+    }
+}
+
+function operatorButtonStuff(currentOperationType, stringOfDigits) {
+    if (currentOperationType == "") {
+        return parseFloat(stringOfDigits)
+    }
+    else {
+        return performOperation(currentTotal, parseFloat(digitsString), operationType)
+    }
+}
 
 /*
 /*
 Done: digitsString is a string variable that concatenates as number buttons and/or the decimal are clicked.
+
 Done: There needs to be a way to check, when the decimal button is clicked, whether or not digitsString already contains a decimal character.
-When a button is clicked, the variable currentTotal = 0 should be set to currentTotal [operator] numberClicked. For now, display currentTotal in the console.
-There needs to be a variable called operationType that will be set to either operationAdd, operationSubtract, operationMultiply, or operationDivide by the click event on the operator buttons.
-I think there needs to be a function called performOperation. It will contain a switch/case thing for each operation, and will return the new value of currentTotal.
+
+Done: When an operator button is clicked, digitsString should be parsed as a float and, if operationType == "", the parsed digitsString float should be saved as currentTotal, else the parsed digitsString float should be operated upon by performOperation function according to current operationType and the result should be saved as currentTotal. Then digitsString should be set to "". operationType updates now to the button clicked. Display currentTotal.
+
+Done: There needs to be a variable called operationType that will be set to either operationAdd, operationSubtract, operationMultiply, or operationDivide by the click event on the operator buttons.
+
+Done: I think there needs to be a function called performOperation. It will contain a switch/case thing for each operation, and will return the new value of currentTotal. I also added an operatorButtonStuff to cut down on some repetitive code that I would have needed to type under every operator button. I could probably cut this down further but I wanted to make sure I could get something working first. 
+
+Still needs: clear button and = button functionality.
 
 */
